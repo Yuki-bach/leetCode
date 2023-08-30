71bbdfa138aadc6616b3681aa2f717783eb11a21
Yuki-bach <yuki20010221@icloud.com>
2023-08-30 08:20:34 -0700

    20230830

---
+++ 238.product-of-array-except-self.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=238 lang=python3
+#
+# [238] Product of Array Except Self
+#
+
+# @lc code=start
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length=len(nums)
+        sol=[1]*length
+        pre = 1
+        post = 1
+        for i in range(length):
+            sol[i] *= pre
+            pre = pre*nums[i]
+            sol[length-i-1] *= post
+            post = post*nums[length-i-1]
+        return(sol)
+
+'''
+22/22 cases passed (201 ms)
+Your runtime beats 63.4 % of python3 submissions
+Your memory usage beats 39 % of python3 submissions (24.2 MB)
+'''
+# @lc code=end
